3676b92a51797f39549da51bca2a68fdc8cd5be5
Priti <p6102267@gaiml.com>
2026-08-13 22:59:42 +0530

    Add mergeSort.cpp practice

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void merge(int arr[], int left, int mid, int right) {
+void merge(int a[], int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
@@ -9,20 +9,20 @@ void merge(int arr[], int left, int mid, int right) {
 
     // Copy data to temporary arrays
     for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
+        L[i] = a[left + i];
 
     for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
+        R[j] = a[mid + 1 + j];
 
     // Merge the temporary arrays
     int i = 0, j = 0, k = left;
 
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
-            arr[k] = L[i];
+            a[k] = L[i];
             i++;
         } else {
-            arr[k] = R[j];
+            a[k] = R[j];
             j++;
         }
         k++;
@@ -30,38 +30,38 @@ void merge(int arr[], int left, int mid, int right) {
 
     // Copy remaining elements
     while (i < n1) {
-        arr[k] = L[i];
+        a[k] = L[i];
         i++;
         k++;
     }
 
     while (j < n2) {
-        arr[k] = R[j];
+        a[k] = R[j];
         j++;
         k++;
     }
 }
 
-void mergeSort(int arr[], int left, int right) {
+void mergeSort(int a[], int left, int right) {
     if (left < right) {
         int mid = left + (right - left) / 2;
 
-        mergeSort(arr, left, mid);      // Left half
-        mergeSort(arr, mid + 1, right); // Right half
+        mergeSort(a, left, mid);      // Left half
+        mergeSort(a, mid + 1, right); // Right half
 
-        merge(arr, left, mid, right);   // Merge both halves
+        merge(a, left, mid, right);   // Merge both halves
     }
 }
 
 int main() {
-    int arr[] = {38, 27, 43, 3, 9, 82, 10};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int a[] = {38, 27, 43, 3, 9, 82, 10};
+    int n = sizeof(a) / sizeof(a[0]);
 
-    mergeSort(arr, 0, n - 1);
+    mergeSort(a, 0, n - 1);
 
     cout << "Sorted array: ";
     for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+        cout << a[i] << " ";
 
     return 0;
 }
