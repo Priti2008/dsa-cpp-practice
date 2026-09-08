e4be51b46a98c5083aeffcb09e61761f3ef045bb
Priti2008 <p6102267@gmail.com>
2026-09-08 09:19:02 +0530

    least k element

---
+++ least k element.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    unordered_map<int, int> freq;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        freq[x]++;
+    }
+
+    int k;
+    cin >> k;
+
+    vector<int> elements;
+
+    for (auto p : freq) {
+        elements.push_back(p.first);
+    }
+
+sort(elements.begin(), elements.end());
+
+
+    elements.resize(k);
+
+reverse(elements.begin(), elements.end());
+
+    cout << "[";
+
+    for (int i = 0; i < k; i++) {
+        cout << elements[i];
+
+        if (i != k - 1)
+            cout << ", ";
+    }
+
+    cout << "]";
+
+    return 0;
+}
