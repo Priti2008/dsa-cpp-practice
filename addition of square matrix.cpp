5800b9d75be80db1db5f3509fdca543b91d8966e
Priti2008 <p6102267@gmail.com>
2026-09-10 23:19:19 +0530

    multiplication

--- addition of square matrix.cpp
+++ addition of square matrix.cpp
@@ -1,12 +1,25 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n=arr.size();
-    vector<int> arr(n);
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            arr[i][j]+=arr[i][j];
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n=arr.size();
+//     vector<int> arr(n);
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             arr[i][j]+=arr[i][j];
+//         }
+//     }
+//     cout<<arr;
+// }
+
+class Solution {
+  public:
+    void addMat(vector<vector<int>>& a, vector<vector<int>>& b) {
+        int n=a.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                a[i][j]+=b[i][j];
+            }
         }
+        
     }
-    cout<<arr;
-}
+};
