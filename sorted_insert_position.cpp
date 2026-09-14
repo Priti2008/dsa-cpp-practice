9cabc11ba186b8db2325711bbca70184b1fd3cfc
Priti2008 <p6102267@gmail.com>
2026-09-14 23:05:56 +0530

    sorted insert position

---
+++ sorted_insert_position.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int k;
+    cin>>k;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]>=k){
+            cout<<i<<endl;
+        }
+    }
+    cout<<n<<endl;
+}
