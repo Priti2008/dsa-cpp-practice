efd131fffa52f6a6fdf6f9c5e0ae969b5e4c67a0
Priti2008 <p6102267@gmail.com>
2026-09-16 10:24:14 +0530

    single number using brute force approach

---
+++ single_number2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	vector<int> arr(n);
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	sort(arr.begin(),arr.end());
+	for(int i=0;i<n;i++){
+		if((i==0 || arr[i]!=arr[i-1]) && (i==n-1 ||arr[i]!=arr[i+1])){
+			cout<<arr[i];
+			break;
+			
+		}
+	}
+}
+
