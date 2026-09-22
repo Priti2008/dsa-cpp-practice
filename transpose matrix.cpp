d2a93fa878b878d0ef7718a6282c333000a1965e
Priti2008 <p6102267@gmail.com>
2026-09-22 15:17:20 +0530

    transpose of matrix

--- transpose matrix.cpp
+++ transpose matrix.cpp
@@ -1,3 +1,18 @@
+// class Solution {
+// public:
+//     vector<vector<int>> transpose(vector<vector<int>>& mat) {
+//         int m=mat.size();
+//         int n=mat[0].size();
+//         vector<vector<int>> result(n,vector<int>(m));
+//         for(int r=0;r<m;r++){
+//             for(int c=0;c<n;c++){
+//                 result[c][r]=mat[r][c];
+//             }
+//         }
+//         return result;
+        
+//     }
+// };
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& mat) {
