4c99fdb7d1870866bc7e04035ee5f3ead5c35707
Priti2008 <p6102267@gmail.com>
2026-09-24 18:33:44 +0530

    maximum subarray

--- maximum_XOR_subarray.cpp
+++ maximum_XOR_subarray.cpp
@@ -1,9 +1,37 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     vector<int >arr(n);
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int maxi=-1;
+//     int start=0;
+//     int end=0;
+//     for(int i=0;i<n;i++){
+//         int current=0;
+//     for(int j=i;j<n;j++){
+//         current^=arr[j];
+//         if(current>maxi){
+//             maxi=current;
+//             start=i;
+//             end=j;
+//         }
+//     }
+//     }
+//     cout<<maxi<<" "<<start<<" "<< end<<endl;
+// }
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
     int n;
     cin>>n;
-    vector<int >arr(n);
+    vector<int> arr(n);
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
@@ -11,15 +39,15 @@ int main(){
     int start=0;
     int end=0;
     for(int i=0;i<n;i++){
-        int current=0;
-    for(int j=i;j<n;j++){
-        current^=arr[j];
-        if(current>maxi){
-            maxi=current;
-            start=i;
-            end=j;
+        int curr=0;
+        for(int j=i;j<n;j++){
+            curr^=arr[j];
+            if(curr>maxi){
+                maxi=curr;
+                start=i;
+                end=j;
+            }
         }
     }
-    }
-    cout<<maxi<<" "<<start<<" "<< end<<endl;
+    cout<<maxi<<" "<<start<<" "<<end<<endl;
 }
