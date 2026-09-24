4f3189beec15b919b328a6b5753a29d7c7bf8d24
Priti2008 <p6102267@gmail.com>
2026-09-24 18:26:00 +0530

    maximum Xor subarray

---
+++ maximum_XOR_subarray.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int >arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int maxi=-1;
+    int start=0;
+    int end=0;
+    for(int i=0;i<n;i++){
+        int current=0;
+    for(int j=i;j<n;j++){
+        current^=arr[j];
+        if(current>maxi){
+            maxi=current;
+            start=i;
+            end=j;
+        }
+    }
+    }
+    cout<<maxi<<" "<<start<<" "<< end<<endl;
+}
